b8cce8d501178682656f8a1fc1d3d12e4b347faa
Christian Sánchez <christian.sanchez@kantox.com>
2023-02-16 21:54:27 +0100

    add rental image component

--- tests/integration/components/rental/image-test.js
+++ tests/integration/components/rental/image-test.js
@@ -1,6 +1,6 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'super-rentals/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { render, click } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
 
 module('Integration | Component | rental/image', function (hooks) {
@@ -23,5 +23,24 @@ module('Integration | Component | rental/image', function (hooks) {
       .dom('[data-test-img] > img')
       .hasAttribute('src', '/assets/images/teaching-tomster.png');
     assert.dom('[data-test-img] > img').hasAttribute('alt', 'Teaching Tomster');
+  });
+
+  test('clicking on the component toggles its modal', async function (assert) {
+    await render(hbs`
+      <Rental::Image
+        src="/assets/images/teaching-tomster.png"
+        alt="Teaching Tomster"
+      />
+    `);
+
+    assert.dom('[data-test-modal]').doesNotExist('Modal is not rendered');
+
+    await click('[data-test-img]');
+
+    assert.dom('[data-test-modal]').exists('Modal is rendered');
+
+    await click('[data-test-close-modal]');
+
+    assert.dom('[data-test-modal]').doesNotExist('Modal is not rendered');
   });
 });
